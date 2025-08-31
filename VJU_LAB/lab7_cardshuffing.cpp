9e83bc1670576e6d2628cfd943ec05bacea89b07
Kaz Tran <kaztran198@gmail.com>
2025-09-01 00:06:10 +0700

    1st commit

--- VJU_LAB/lab7_cardshuffing.cpp
+++ VJU_LAB/lab7_cardshuffing.cpp
@@ -1,42 +1,47 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+#define fl ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define Piu signed main()
+#define fi first
+#define se second
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vl vector<ll>
+#define vvl vector<vl>
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define allr(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int) x.size())
+#pragma GCC optimize ("Ofast","unroll-loops")
 using namespace std;
-
-int main() {
-    int n;
-    cout << "Nhập vào số nguyên dương n (n<200): ";
+typedef long long ll;
+typedef unsigned long long ull;
+typedef int_fast64_t fint;
+const ll maxN = 500005, lim = 1e6 + 5, bits = 30, inf = 1 << bits, MOD = 998244353; 
+Piu {
+    fl;
+    ll n;
     cin >> n;
-    
-    int total = 2 * n;
-    vector<int> initial;
-    for (int i = 1; i <= total; i++) {
-        initial.push_back(i);
-    }
-    
-    vector<vector<int>> states;
-    states.push_back(initial);
-    
-    vector<int> current = initial;
+    ll t = 2 * n;
+    vl a;
+    for (ll i = 1; i <= t; i++) a.push_back(i);
+    vvl v;
+    v.push_back(a);
+    vl cur = a;
     while (true) {
-        vector<int> newDeck;
-        for (int i = 0; i < n; i++) {
-            newDeck.push_back(current[n + i]);
-            newDeck.push_back(current[i]);
+        vl nd;
+        for (ll i = 0; i < n; i++) {
+            nd.push_back(cur[n + i]);
+            nd.push_back(cur[i]);
         }
-        current = newDeck;
-        states.push_back(current);
-        if (current == initial) {
-            break;
-        }
+        cur = nd;
+        v.push_back(cur);
+        if (cur == a) { break; }
     }
-    
-    cout << states.size() - 1 << endl;
-    for (const auto& state : states) {
-        for (int card : state) {
-            cout << card << " ";
-        }
-        cout << endl;
+    cout << v.size() - 1 << '\n';
+    for (const auto& x: v) {
+        for (ll card : x) cout << card << " ";
+        cout << '\n';
     }
-    
-    return 0;
 }
